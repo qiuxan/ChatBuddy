5f76edc51631c8c287550bd7d8a63ba74797871b
qiuxan <qiuxan@gmail.com>
2026-06-04 18:37:29 +1000

    test open-webui in local env.

--- ChatBuddy.AppHost/AppHost.cs
+++ ChatBuddy.AppHost/AppHost.cs
@@ -1,4 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama=builder.AddOllama("ollama").WithDataVolume();
+ollama.AddModel("chat", "llama3.2");
+/*
+builder
+    .AddContainer("open-webui", "ghcr.io/open-webui/open-webui", "main")
+    .WithHttpEndpoint(port: 3000, targetPort: 8080, name: "http")
+    .WithEnvironment("OLLAMA_BASE_URL", ollama.GetEndpoint("http"))
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WaitFor(ollama);
+ */   
 builder.AddProject<Projects.ChatAPI>("ChatAPI");
 builder.Build().Run();
