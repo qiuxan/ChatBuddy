29ce1a84b03dc055fdf41c7bb6e4fa24d2b9c42a
qiuxan <qiuxan@gmail.com>
2026-06-07 08:23:16 +1000

    Setup local LLM instead of Docker Ollama to solve Docker Ollarma Mac performance issue

--- ChatBuddy.AppHost/AppHost.cs
+++ ChatBuddy.AppHost/AppHost.cs
@@ -1,7 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var ollama=builder.AddOllama("ollama-service").WithDataVolume();
-var chatModel= ollama.AddModel("chat-service", "llama3.2:1b");
+var chatModel = builder.AddConnectionString("chat-service");
+
+/*
+var ollama = builder.AddOllama("ollama-service").WithDataVolume();
+var chatModel = ollama.AddModel("chat-service", "llama3.2:1b");
+*/
 /*
 builder
     .AddContainer("open-webui", "ghcr.io/open-webui/open-webui", "main")
@@ -11,7 +15,6 @@ builder
     .WaitFor(ollama);
  */   
 builder.AddProject<Projects.ChatAPI>("ChatAPI")
-    .WithReference(chatModel)
-    .WaitFor(chatModel);
+    .WithReference(chatModel);
 
 builder.Build().Run();
