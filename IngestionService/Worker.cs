2c5c0ba7158c62a2f700ab2f6d4b356a1e34377b
qiuxan <qiuxan@gmail.com>
2026-06-09 09:21:45 +1000

    Impelement Reader to the Ingestion worker

--- IngestionService/Worker.cs
+++ IngestionService/Worker.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
+using Microsoft.Extensions.DataIngestion;
+
 namespace IngestionService;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILoggerFactory loggerFactory,ILogger<Worker> logger) : BackgroundService
 {
     const string trackingFilePath = "tracking.txt";
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,10 +24,37 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 logger.LogInformation("Files to process: {files}", string.Join(',', filesProcess.Select(x => x.Name)));
             }
-            
+
+            var pipeline = new IngestionPipeline<string>(
+                reader:new IncReader(),
+                chunker:null,
+                writer:null,
+                loggerFactory: loggerFactory
+                );
+
+            await foreach (var result in pipeline.ProcessAsync(filesProcess, stoppingToken))
+            {
+                if (!result.Succeeded)
+                {
+                    logger.LogError("Failed to process: {filesId} ",result.DocumentId);
+                }
+            }
+
             await File.AppendAllLinesAsync(trackingFilePath, filesProcess.Select(_ => _.FullName), stoppingToken);
 
             await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    public class IncReader : IngestionDocumentReader
+    {
+        readonly MarkdownReader _markdownReader = new();
+        public override Task<IngestionDocument> ReadAsync(Stream source, string identifier, string mediaType,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            Debug.WriteLine(identifier);
+            Debug.WriteLine(mediaType);
+            return _markdownReader.ReadAsync(source,identifier,mediaType,cancellationToken);
         }
     }
 }
